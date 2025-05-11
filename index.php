9e447cea84869641ed910713051fe3b2e7c9893d
Elmir Mamishev <elmir.mamishev31@gmail.com>
2025-05-11 15:57:23 +0200

    Hello, Dima

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Привет, PHP!</title>
+</head>
+<body>
+    <h1><?php echo "Добро пожаловать на мой сайт!"; ?></h1>
+</body>
+</html>
